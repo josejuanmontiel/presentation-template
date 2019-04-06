dcc73838011d9dae964e69b46daf5f4a710fdbe2
Jorge Aguilera <jorge.aguilera@puravida-software.com>
2019-04-06 20:51:37 +0200

    add menu

--- src/docs/resources/reveal.js/revealjs-plugins.js
+++ src/docs/resources/reveal.js/revealjs-plugins.js
@@ -1,3 +1,4 @@
 { src: 'reveal.js/plugin/rajgoel/chart/Chart.min.js' },
 { src: 'reveal.js/plugin/rajgoel/chart/csv2chart.js' },
 { src: 'reveal.js/plugin/rajgoel/chalkboard/chalkboard.js' },
+{ src: 'reveal.js/plugin/denehyg/reveal.js-menu/menu.js' },
